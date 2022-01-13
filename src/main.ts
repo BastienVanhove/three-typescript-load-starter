ab8374fd98735e0e2d8253cab7e930437050a82c
BastienVanhove <bastienv.capital@gmail.com>
2022-01-13 11:34:12 -0500

    first style of loading thing

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import './assets/styles/style.css'
 
-const loadPage = document.querySelector<HTMLDivElement>('.load-page')
 for(let i = 0; i<10000; i++){
   console.log(i)
 }
-window.addEventListener('load',()=>{
+
+const endLoading = () =>{
+  const loadPage = document.querySelector<HTMLDivElement>('.load-page')
   if(loadPage) loadPage.style.opacity = '0'
   setTimeout(()=>{
     if(loadPage) loadPage.style.display = 'none'
   },500)
-})
+}
+window.addEventListener('load', endLoading)
 //const app = document.querySelector<HTMLDivElement>('#app')!
 
