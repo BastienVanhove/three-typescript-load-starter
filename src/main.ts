ff9933a7e75101fe9c3b978b73685664ba745c87
BastienVanhove <bastienv.capital@gmail.com>
2022-01-13 11:36:10 -0500

    first style of loading thing

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,5 @@
 import './assets/styles/style.css'
 
-for(let i = 0; i<10000; i++){
-  console.log(i)
-}
-
 const endLoading = () =>{
   const loadPage = document.querySelector<HTMLDivElement>('.load-page')
   if(loadPage) loadPage.style.opacity = '0'
